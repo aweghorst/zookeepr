309af9c021cbda8e0683ea18d626f95dc553d2ed
aw2389 <alex2@weghorst.com>
2021-05-26 16:08:48 -0500

    updated with final code from module

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,30 +1,28 @@
-const $displayArea = document.querySelector("#display-area");
-const $zookeeperForm = document.querySelector("#zookeeper-form");
+const $zookeeperForm = document.querySelector('#zookeeper-form');
+const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
   console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
+  const zookeeperHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
       <h4 class="text-primary">${name}</h4>
       <p>Age: ${age}<br/>
-      Favorite Animal: ${
-        favoriteAnimal.substring(0, 1).toUpperCase() +
-        favoriteAnimal.substring(1)
-      }<br/>
+      Favorite Animal: ${favoriteAnimal.substring(0, 1).toUpperCase() +
+      favoriteAnimal.substring(1)}<br/>
       </p>
     </div>
   </div>
     `;
   });
 
-  $displayArea.innerHTML = animalHTML.join("");
+  $displayArea.innerHTML = zookeeperHTML.join('');
 };
 
 const getZookeepers = (formData = {}) => {
-  let queryUrl = "/api/zookeepers?";
+  let queryUrl = '/api/zookeepers?';
 
   Object.entries(formData).forEach(([key, value]) => {
     queryUrl += `${key}=${value}&`;
@@ -56,5 +54,6 @@ const handleGetZookeepersSubmit = event => {
   getZookeepers(zookeeperObject);
 };
 
-$zookeeperForm.addEventListener("submit", handleGetZookeepersSubmit);
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
+
 getZookeepers();
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,8 @@
-const router = require("express").Router();
-const animalRoutes = require("../apiRoutes/animalRoutes");
-const zooKeeperRoutes = require("../apiRoutes/zookeeperRoutes");
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 router.use(animalRoutes);
-router.use(zooKeeperRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
